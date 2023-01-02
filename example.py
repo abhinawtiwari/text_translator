b58acc3b16f1bf0dc275efefc539ccd66fa3037f
Abhinaw Tiwari <abhinawtiwari@Abhinaws-MacBook-Pro.local>
2023-01-02 21:35:46 +0000

    added small example

---
+++ example.py
@@ -0,0 +1,5 @@
+from deep_translator import GoogleTranslator
+print(GoogleTranslator(source='auto', target='en').translate("es un lugar acogedor con teresa su esposo que nos atendieron explendidamente mi hijo yo la pasamos muy bien lo recomiendo mucho 😁😁"))
+# 'weiter so, du bist toll'
+
+# Great space that has been given  the right touch. Nice wee home 😁😁
